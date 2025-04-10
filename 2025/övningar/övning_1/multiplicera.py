e8c6207605de3bcd89f441d87884d10a8d43df16
averyiaa <aaveryia@gmail.com>
2025-04-10 23:40:06 +0200

    test comment

--- 2025/övningar/övning_1/multiplicera.py
+++ 2025/övningar/övning_1/multiplicera.py
@@ -4,3 +4,5 @@ tal = int(input('Skriv in ett heltal: '))
 multiplicerat_tal = tal * 3
 # Skriv ut det multiplcerade talet
 print(f'Talet {tal} multiplicerat med 3 blir {multiplicerat_tal}.')
+
+# testestst
