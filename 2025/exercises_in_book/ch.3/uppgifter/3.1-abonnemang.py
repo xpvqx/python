17daa2308d4ed4bdae52322c92b8569206c16214
xpxqv <aaveryia@gmail.com>
2025-04-18 02:59:44 +0200

    practice

--- 2025/exercises_in_book/ch.3/uppgifter/3.1-abonnemang.py
+++
@@ -1,9 +0,0 @@
-m = int(input('Hur många minuter?: '))
-p = int(input('Hur mycket kostar det per minut?: '))
-t = m * p
-
-if t >= 300:
-    t = t * 0.90
-    print(f'Det kostar {t} kr i månaden. Du har rabatt. ')
-else:
-    print(f'Det kostar {t} kr i månaden. Du har ej rabatt. ')
--- 2025/moduler/modul_3_strängar/vokaler.py
+++
@@ -1,7 +0,0 @@
-s = 'Some sensitive word'
-
-for c in s:
-    if c in 'aeiouy':
-        print('*', end='')
-    else:
-        print(c, end='')
