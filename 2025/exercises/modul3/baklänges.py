02de2a3026068a93ab9c272fe69e7b6a1ae30f9f
averyiaa <aaveryia@gmail.com>
2025-04-18 10:49:10 +0200

    sss

---
+++ 2025/exercises/modul3/baklänges.py
@@ -0,0 +1,5 @@
+word = input('Skriv in ett ord: ')
+reversed_word = word[::-1]
+
+for c in reversed_word:
+    print(c)
---
+++ 2025/exercises/modul3/baklänges2.py
@@ -0,0 +1,5 @@
+word = input('Enter a word: ')
+reversed_word = word[::-1]
+
+for c in reversed_word:
+    print(c)
