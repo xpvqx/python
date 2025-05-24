170a11c83b28e434b6bc164d9daf772339e561ef
xpvqx <149103142+xpvqx@users.noreply.github.com>
2025-05-24 17:38:41 +0200

    Delete 2025/moduler/modul_3_strängar directory

--- 2025/moduler/modul_3_strängar/vokaler.py
+++
@@ -1,7 +0,0 @@
-s = 'Some sensitive information'
-
-for c in s:                 # c är ett variable namn vi har valt, spara alla karaktärer i s(ordet)
-    if c in 'aeiouy':       # om c är en volak
-        print('*', end='')  # print '*'
-    else:
-        print(c, end='')    # annars printa c = character i s
